048a2edb6000e74e64df46f7debdaa65de0c17ff
Aaradhya Deotale <102856132+AaradhyaDeotale@users.noreply.github.com>
2023-05-28 22:56:11 +0530

    Day 2- All Levels
    So, Day 2 is looking pretty good! Im confident ill go through with all the 30 days!

---
+++ Day 2/day2level1.py
@@ -0,0 +1,15 @@
+#Day 2: 30 Days of Python Programming Level 1
+
+first_name = 'Aaradhya'
+last_name = 'Deotale'
+full_name = 'Aaradhya G Deotale'
+country = 'India'
+city = 'Mumbai'
+age = 20
+year = 2003
+is_married = False
+is_true = False
+is_light_on = True
+a, b ,c = 10,20,30
+
+
---
+++ Day 2/day2level2.py
@@ -0,0 +1,41 @@
+#Day 2: 30 Days of Python Programming Level 2
+first_name = 'Aaradhya'
+last_name = 'Deotale'
+full_name = 'Aaradhya G Deotale'
+country = 'India'
+city = 'Mumbai'
+age = 20
+year = 2003
+is_married = False
+is_true = False
+is_light_on = True
+a, b ,c = 10,20,30
+
+print(type(first_name))
+print(type(is_true))
+print(type(age))
+print(len(first_name))
+
+num_one, num_two = 5,4
+sum = num_one + num_two
+print(sum)
+diff = num_one - num_two
+print(diff)
+mul = num_one * num_two
+print(mul)
+div = num_one / num_two
+print(div)
+
+circleradius = 30
+area_circle = 3.14 * (circleradius)**2
+print(area_circle)
+circum_circle = 2*3.14*circleradius
+print(circum_circle)
+
+fname = input('Enter your name: ')
+print("Hello, " + fname + "!")
+
+Country = input("So," + fname + " which country are you from?: ")
+
+print("Oh, thats amazing! I love " + Country + "!")
+
